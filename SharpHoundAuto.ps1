5e74452d98c50e0fc8eba8d4b32ac9cfcef0058a
cottinghamd <dcottingham@foresightconsulting.com.au>
2018-11-13 10:11:44 +1100

    Update for latest Sharphound using the All flag
    Since the new version of SharpHound support the -all collection method and automatically outputs domain results to zip files, we no longer need to create directories for each scan to contain the files, or run three different sharphound commands for collection.

--- SharpHoundAuto.ps1
+++ SharpHoundAuto.ps1
@@ -10,61 +10,39 @@ $domainlist = "$working\targetdomains.txt"
 
 #check for administrator privileges
 If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
- 
 {
-write-host "This script requires administrative privileges which have not been detected. This script will now exit.`r`nPlease start a powershell prompt as an administrator and run the script again. `r`n" -ForegroundColor Yellow
-Pause
-Break
+	write-host "This script requires administrative privileges which have not been detected. This script will now exit.`r`nPlease start a powershell prompt as an administrator and run the script again. `r`n" -ForegroundColor Yellow
+	Pause
+	Break
 }
- 
+
 #ensure sharphound exists in the current working directory
 If (Test-Path -Path $sharphoundlocation -ErrorAction SilentlyContinue)
 {
-write-host "Sharphound found $sharphoundlocation"
+	write-host "Sharphound found $sharphoundlocation"
+	
+	#ensure a domain list exists in the current working directory
+	If (Test-Path -Path $domainlist -ErrorAction SilentlyContinue)
+	{
+		write-host "Domain list found $domainlist"
+		#read in the domain list from the text file and loop through all domains
+		Get-Content $domainlist | ForEach-Object{
+				write-host "Commencing collection for $_" -ForegroundColor Magenta
 
-#ensure a domain list exists in the current working directory
-    If (Test-Path -Path $domainlist -ErrorAction SilentlyContinue)
-    {
-        write-host "Domain list found $domainlist"
-        #read in the domain list from the text file and loop through all domains
-        Get-Content $domainlist | ForEach-Object{
- 
-        #if a subdirectory with the domain name already exists, skip scanning the domain
-        If (Test-Path -Path "$working\$_" -ErrorAction SilentlyContinue)
-        {
-            write-host "Skipping scan of $_ as results directory already exists" -ForegroundColor Yellow
-        }
-        else
-        {
-            write-host "Commencing collection for $_" -ForegroundColor Magenta
-            #create a subdirectory with the domains name
-            New-Item -Path "$working\$_" -ItemType directory |Out-Null
-            #test that the folder exists before proceeding
-                If (Test-Path -Path "$working\$_" -ErrorAction SilentlyContinue)
-                {
-                    #send commands to sharphound
-                    Invoke-Expression "./sharphound.exe -d $_ --CSVFolder `"$working\$_`" --CSVPrefix `"$_`""
-                    Invoke-Expression "./sharphound.exe -d $_ -c ObjectProps --CSVFolder `"$working\$_`" --CSVPrefix `"$_`""
-                    Invoke-Expression "./sharphound.exe -d $_ -c ACL --CSVFolder `"$working\$_`" --CSVPrefix `"$_`""
-                    Invoke-Expression "./sharphound.exe -d $_ -c Container --CSVFolder `"$working\$_`" --CSVPrefix `"$_`""
-                    write-host "Collection of $_ complete, files written to $working\$_\" -ForegroundColor Green
-                }
-                else
-                {
-                    write-host "Directory creation for $_ failed"
-                }
- 
-         }
-       }
-     }
-     else
-     {
-        write-host "Domain List Not Found, expected $domainlist" -ForegroundColor Red
-        pause
-     }
+				#send commands to sharphound
+				Invoke-Expression "./sharphound.exe -d $_ -c All"
+				write-host "Collection of $_ complete, files written to $working\" -ForegroundColor Green
+		}
+	}
+	else
+	{
+		write-host "Domain List Not Found, expected $domainlist" -ForegroundColor Red
+		pause
+	}
 }
 else
 {
-    write-host "Sharphound Not Found, expected $sharphoundlocation" -ForegroundColor Red
-    pause
+	write-host "Sharphound Not Found, expected $sharphoundlocation" -ForegroundColor Red
+	pause
 }
+
